69a3e0ca5538adf2e954fb37970e6979145e3c61
Ali Sheikh <ali.sheikh@flexport.com>
2020-11-26 00:39:46 -0800

    testing

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/auth.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3"
+    }
+  },
+
+};
