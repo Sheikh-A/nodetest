4dd0487350822de244357e02be9d14893cbfd04c
Ali Sheikh <ali.sheikh@flexport.com>
2020-11-26 00:55:27 -0800

    adding get

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.status(200).send((`<h1>API: Up Up and Away, welcome Flexporters!</h1>`))
+})
+
 module.exports = server;
