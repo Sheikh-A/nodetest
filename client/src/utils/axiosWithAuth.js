ad24eab9956038da49493c896a336801e98716e5
Ali Sheikh <ali.sheikh@flexport.com>
2020-11-26 02:02:36 -0800

    auth

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: "https://astesting123.herokuapp.com/api",
+
         headers: {
             "content-type": "application/json",
             Authorization: JSON.parse(token)
