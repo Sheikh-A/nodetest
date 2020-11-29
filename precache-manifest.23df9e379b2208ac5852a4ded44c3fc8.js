b28bbe28e84aad84b40155830b650015cae9f9e7
Ali Sheikh <ali.sheikh@flexport.com>
2020-11-29 02:36:39 -0800

    Updates

---
+++ precache-manifest.23df9e379b2208ac5852a4ded44c3fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde23856cab50db14e5e70d9aa8ce1b1",
+    "url": "/Sheikh-A/nodetest/index.html"
+  },
+  {
+    "revision": "4af4ae84cde9811567a4",
+    "url": "/Sheikh-A/nodetest/static/css/main.d3b4e434.chunk.css"
+  },
+  {
+    "revision": "39f949342a9dc5e29ba5",
+    "url": "/Sheikh-A/nodetest/static/js/2.7c48930e.chunk.js"
+  },
+  {
+    "revision": "4af4ae84cde9811567a4",
+    "url": "/Sheikh-A/nodetest/static/js/main.650029a1.chunk.js"
+  },
+  {
+    "revision": "cbd340f4c76171242cc0",
+    "url": "/Sheikh-A/nodetest/static/js/runtime-main.c8f531ba.js"
+  },
+  {
+    "revision": "06dfc87640472e47ae6272b20471b2bc",
+    "url": "/Sheikh-A/nodetest/static/media/dots.06dfc876.svg"
+  },
+  {
+    "revision": "40f1a854f76da053e64c890566a98f16",
+    "url": "/Sheikh-A/nodetest/static/media/logo.40f1a854.png"
+  }
+]);
